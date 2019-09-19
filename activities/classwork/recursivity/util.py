0720f4fba3289f124a43e52f27b8221ebfa98d7c
mauanaya <vmauricio.anaya@gmail.com>
2019-09-18 19:16:45 -0500

    Create recursive implementation

--- activities/classwork/recursivity/util.py
+++ activities/classwork/recursivity/util.py
@@ -1,21 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  4 19:00:49 2019
+
+@author: mauricioanaya
+"""
 import functools
 import time
 
 
+
+def caching(func):
+    CACHE = {}
+    @functools.wraps(func)
+    def wrapper(self, **kwargs):
+        key = hash(frozenset(kwargs.items()))
+        if key in CACHE:
+            return CACHE[key]
+        CACHE[key] = func(self, **kwargs)
+        return wrapper(self, **kwargs)
+    return wrapper
+
+
+
+def timeit(logger):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(self, *args, **kwargs):
+            start = time.time()
+            output = func(self, *args, **kwargs)
+            end = time.time()
+            logger.info("Start time %s End time %s" % (start, end))
+            logger.info("Execution time %s" % (end - start))
+            return output
+        return wrapper
+    return decorator
+
+            
+
+def _corner_case_decorator(func):
+    def wrapper(self, i, j, *args, **kwargs):
+        if j >= i or j == 0:
+            return 1
+        return func(self, i=i, j=j, *args, **kwargs)
+    return wrapper
+
+
 class TriangleBuilder(object):
     CACHE = {}
-
-    def save(self, i, j, value):
-        pass
-
-    def get(self, i, j, default=None):
-        pass
-
+    
+    def save(self, i , j, value):
+        self.CACHE[(i, j)] = lambda: value
+        return value
+    
+    @_corner_case_decorator
+    def get(self, i, j, default=lambda: None):
+        return self.CACHE.get((i, j), default)()
+    
+    @_corner_case_decorator
     def create(self, i, j):
-        pass
-
+        if j >= i or j == 0:
+            return 1
+        upper_left = self.get_or_create(i=i-1, j=j-1)
+        upper_center = self.get_or_create(i=i-1, j=j)
+        return self.save(i=i, j=j, value=upper_left+upper_center)
+        
+    
     def get_or_create(self, i, j):
-        pass
-
+        return self.get(i, j, default=lambda: self.create(i=i, j=j))
+    
     def get_row(self, index):
-        pass
+        return [str(self.get_or_create(index, j)) for j in range(index+1)]
+    
+    
