69ea121618591a2ba481cf98f80589c09a9db073
mauanaya <vmauricio.anaya@gmail.com>
2019-09-25 19:51:51 -0500

    Create amortization solution

--- activities/classwork/amortization/main.py
+++ activities/classwork/amortization/main.py
@@ -1,5 +1,4 @@
 import fire
-
 from util import Amortization
 
 
@@ -11,13 +10,24 @@ class Main(object):
 
     @staticmethod
     def annuity(amount, interest, n):
-        pass
+        amortization = Amortization(amount, interest, n)
+        return amortization.annuity
+        
 
     def table(self, amount, interest, n, rows=10, save=""):
-        pass
+        amortization = Amortization(amount, interest, n)
+        df = amortization.get_table()
+        print(df)
+        if save:
+            df.to_csv(save)
+        
 
     def graph(self, amount, interest, n, save=""):
-        pass
+        amortization = Amortization(amount, interest,n)
+        fig = amortization.get_plot()
+        if save:
+            fig.savefig(save)
+        
 
 
 if __name__ == "__main__":
