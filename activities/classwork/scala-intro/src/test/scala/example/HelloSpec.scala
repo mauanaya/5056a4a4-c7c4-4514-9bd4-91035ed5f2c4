44620131625d9f79d9d92c6b1e3917dfebecfb44
mauanaya <46908589+mauanaya@users.noreply.github.com>
2019-12-04 12:32:06 -0600

    Update HelloSpec.scala

--- activities/classwork/scala-intro/src/test/scala/example/HelloSpec.scala
+++ activities/classwork/scala-intro/src/test/scala/example/HelloSpec.scala
@@ -1,3 +1,4 @@
+
 package example
 
 import org.scalatest._
@@ -5,5 +6,10 @@ import org.scalatest._
 class HelloSpec extends FlatSpec with Matchers {
   "The Hello object" should "say hello" in {
     Hello.greeting().toLowerCase() shouldEqual "hello, world!"
+  }
+
+  it should "say 'hello test-a'" in {
+    val greeting = Hello.greeting(name = "test-a").toLowerCase
+    greeting shouldEqual "hello, test-a!"
   }
 }
