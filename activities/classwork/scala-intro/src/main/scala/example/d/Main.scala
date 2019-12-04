e9a93cb74fa8c4b2a1462bf73aaeb58d0dfa2e2b
mauanaya <46908589+mauanaya@users.noreply.github.com>
2019-12-04 12:28:54 -0600

    Update Main.scala

--- activities/classwork/scala-intro/src/main/scala/example/d/Main.scala
+++ activities/classwork/scala-intro/src/main/scala/example/d/Main.scala
@@ -7,22 +7,29 @@ import example.d.model.Visitor
 
 object Main {
 
-  def older(v1: Visitor, v2: Visitor): Boolean = v1.createdAt.seconds > v2.createdAt.seconds
+  def older(v1: Visitor, v2: Visitor): Boolean =
+    v1.createdAt.seconds > v2.createdAt.seconds
 
-  def getAnonymousUser(age: Int): Visitor = Visitor.Anonymous(
+  def getAnonymousUser(age: Int): Visitor = Visitor.Anonymous( //definimos el metodo
     UUID.randomUUID().toString,
-    Timestamp(1000))
+    Timestamp(age))
 
-  val getUSer: Int => Visitor = (age: Int) = Visitor.User(
+
+  val getUser: Int => Visitor = (age: Int) => Visitor.User( //definimos la funcion
     id = UUID.randomUUID().toString,
     email = "email@example.com",
-    createdAt = Timestamp(900))
+    createdAt = Timestamp(age)
+  )
 
+  // First case: sbt "runMain example.d.Main 100 90"
+  // Second case: sbt "runMain example.d.Main 100 900"
   def main(args: Array[String]): Unit = {
     val Array(firstAge, secondAge) = args
-    val a = getAnonymousUser(firstAge.toInt)
-    val b = getUSer(secondAge.toInt)
 
-    if (older(a,b)) a.show() else b.show()
+    val a = getAnonymousUser(firstAge.toInt)
+    val b = getUser(secondAge.toInt)
+
+    // Print older user
+    if (older(a, b)) a.show() else b.show()
   }
 }
